bfd547d0591c2f4c39a7ce8944076d2484125570
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-09-05 14:06:46 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+import Keypad from './Components/Keypad';
+import Output from './Components/Output';
+
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Kendall's Calculator</h1>
+                    <Output result={this.state.result}/>
+                    <Keypad onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Components/Output.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+
+class Output extends Component{
+
+render() {
+    let {result} = this.props;
+    return (
+        <div className="result">
+            <p>{result}</p>
+        </div>
+)
+    ;
+}
+}
+
+
+
+
+
+export default Output;
